8dc2ffd283a92646822e104e352391163a811443
Prince <princekumarsingh016592@gmail.com>
2026-07-02 12:48:20 +0530

    Vertical Triangle Done

---
+++ Week 3/I_Binary_Pyramid.cpp
@@ -0,0 +1,21 @@
+// Revisit Solution
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<< "Enter Row" << endl;
+    cin>>n;
+
+    bool isOne = true;
+    bool isZero = false;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+          cout<<isOne;
+          cout<<isZero;
+        }
+        cout<<endl;
+    }
+  return 0;
+}
