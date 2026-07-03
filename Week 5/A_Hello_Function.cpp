0bacb636698dfa662cd592b23d5ec937322f0108
Prince <princekumarsingh016592@gmail.com>
2026-07-03 19:51:48 +0530

    I am learning Function

---
+++ Week 5/A_Hello_Function.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void greet(int n){
+  for(int i=1; i<=n; i++){
+    cout<<"I am Learning Functions"<<endl;
+  }
+}
+
+int main(){
+  greet(3);
+return 0;
+}
